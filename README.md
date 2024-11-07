e2ebd6d489dd4a0ce7ed5cc1e77e0a764f54bf4d
simonhuwiler <webmaster@simonhuwiler.ch>
2024-11-07 15:11:52 +0100

    init

---
+++ README.md
@@ -0,0 +1,16 @@
+# Chrome-Extension für NZZ-Grafiken
+Diese Chrome-Erweiterung fügt einen Shortlink bei jeder Q und DataWrapper-Grafik ein und öffnet bei Klick den entsprechenden Editor.
+
+## Installation
+* In Chrome Erweiterungen öffnen: `chrome://extensions/`
+
+
+## Installation für Entwickler
+* In Chrome Erweiterungen öffnen: `chrome://extensions/`
+* Rechts oben `Entiwcklermodus` aktivieren
+* `Entpackte Erweiterung laden` klicken und dieses Verzeichnis auswählen
+
+Nach jeder Änderung am Code muss die Erweiterung über das Pfeilsymbol neu geladen werden
+
+## Maintainer
+shu
---
+++ src/app.js
@@ -0,0 +1,103 @@
+const addDataWrapper = () => {
+
+  const extractDataWrapperId = (url) => {
+    const match = url.match(/datawrapper\.dwcdn\.net\/([^/]+)/);
+    return match ? match[1] : null;
+  };
+
+  document.querySelectorAll('iframe[src*="datawrapper"]').forEach((iframe) => {
+    const parent = iframe.parentNode;
+
+    // Set Position relative to parent
+    parent.style.position = 'relative';
+
+    // Inject HTML
+    parent.insertAdjacentHTML('afterbegin', `<a href="https://app.datawrapper.de/chart/${extractDataWrapperId(iframe.src)}/visualize" target="_blank" class="nzz_extension_edit" alt="Bearbeiten in DataWrapper"></a>`);
+  })
+}
+
+
+const qItems = {
+  "charts": {
+    "selector": ".q-chart-container",
+    "url": "https://qv2.st.nzz.ch/editor/chart/%id%"
+  },
+  "table": {
+    "selector": ".q-table-container",
+    "url": "https://qv2.st.nzz.ch/editor/table/%id%"
+  },
+  "choropleth": {
+    "selector": ".q-choropleth-container",
+    "url": "https://qv2.st.nzz.ch/editor/choropleth/%id%"
+  },
+  "electionVotes": {
+    "selector": ".q-election_votes-container",
+    "url": "https://qv2.st.nzz.ch/editor/coalition_calculation/%id%"
+  },
+  "electionSeats": {
+    "selector": ".q-election_seats-container",
+    "url": "https://qv2.st.nzz.ch/editor/election_seats/%id%"
+  },
+  "electionExecutive": {
+    "selector": ".q-election_executive-container",
+    "url": "https://qv2.st.nzz.ch/editor/election_executive/%id%"
+  },
+  "imageSlider": {
+    "selector": ".q-imageslider-container",
+    "url": "https://qv2.st.nzz.ch/editor/imageslider/%id%"
+  },
+  "infographic": {
+    "selector": ".q-infographic-container",
+    "url": "https://qv2.st.nzz.ch/editor/infographic/%id%"
+  },
+  "locatorMap": {
+    "selector": ".q-locator_map-container",
+    "url": "https://qv2.st.nzz.ch/editor/locator_map/%id%"
+  },
+  "partySlogan": {
+    "selector": ".q-party_slogans-container",
+    "url": "https://qv2.st.nzz.ch/editor/party_slogans/%id%"
+  },
+  "pollResult": {
+    "selector": ".q-poll_result-container",
+    "url": "https://qv2.st.nzz.ch/editor/poll_result/%id%"
+  }
+}
+
+
+const addQ = (props) => {
+
+  document.querySelectorAll(props.selector).forEach((chart) => {
+
+    const parent = chart.parentNode.parentNode;
+
+    // Set Position relative to parent
+    parent.style.position = 'relative';
+
+    // URL
+    url = props.url.replace("%id%", chart.getAttribute("data-q-item-id"));
+
+    // Add HTML Code
+    parent.insertAdjacentHTML('afterbegin', `<a href="${url}" target="_blank" class="nzz_extension_edit" alt="Bearbeten in Q"></a>`);
+  })
+}
+
+setTimeout(() => {
+
+  addDataWrapper();
+
+  addQ(qItems['charts']);
+  addQ(qItems['table']);
+  addQ(qItems['choropleth']);
+  addQ(qItems['choropleth']);
+  addQ(qItems['electionVotes']);
+  addQ(qItems['electionSeats']);
+  addQ(qItems['electionExecutive']);
+  addQ(qItems['imageSlider']);
+  addQ(qItems['infographic']);
+  addQ(qItems['locatorMap']);
+  addQ(qItems['partySlogan']);
+  addQ(qItems['pollResult']);
+
+}, 500)
+
